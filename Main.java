9ded10edd28f6d4a6f327bcce4ad2147d4c0dbb6
Stu Thompson <stuart@stuartthompson.net>
2018-04-04 15:26:49 -0700

    Removes Main.java (moving to Android)
    Main class no longer necessary.

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("CouchBase Lite Event Updates");
-        System.out.println("============================");
-        System.out.println("A simple test of the event update systems within Couchbase Lite.");
-    }
-}
