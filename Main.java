b61654e45e5257aaeb207ac019254f6d2a212758
Stu Thompson <stuart@stuartthompson.net>
2018-04-04 14:26:42 -0700

    Updates placeholder with new project name.

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("SQLite Triggers");
-        System.out.println("===============");
-        System.out.println("A simple test of the trigger systems within SQLite.");
+        System.out.println("CouchBase Lite Event Updates");
+        System.out.println("============================");
+        System.out.println("A simple test of the event update systems within Couchbase Lite.");
     }
 }
